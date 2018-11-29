71a93991f194a4328ae3c1b98ffd53774fbb5f86
markobalogh <12981558+markobalogh@users.noreply.github.com>
2018-11-28 20:33:09 -0800

    seems to be working :-)

--- src/background.js
+++ src/background.js
@@ -1,3 +1,14 @@
+var host = "http://smile.amazon.com";
 chrome.webRequest.onBeforeRequest.addListener(
-    
-)
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://amazon.com/*",
+            "*://www.amazon.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
